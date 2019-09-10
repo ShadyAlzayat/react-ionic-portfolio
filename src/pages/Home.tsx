6966f2e855d08cd04ee958a2bd1736b149fa85b4
Shady Alzayat <emailshady@gmail.com>
2019-09-09 17:39:17 -0700

    fix case sensitive link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,7 +74,7 @@ const HomePage: React.FunctionComponent = () => {
             <div className='intro-slides-text-box'>
               <h1 className='intro-slides-text'>Download my resume.</h1>
               <IonButton
-                href='`${process.env.PUBLIC_URL}/assets/shady_alzayat.docx`'
+                href='`${process.env.PUBLIC_URL}/assets/Shady_Alzayat.docx`'
                 download='shady_alzayat.docx'
                 fill='outline'
                 color='dark'
