90afd372f1f289689d925619a9bfd03dda394d07
Shady Alzayat <emailshady@gmail.com>
2019-09-09 18:01:37 -0700

    fixed file download link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -74,7 +74,7 @@ const HomePage: React.FunctionComponent = () => {
             <div className='intro-slides-text-box'>
               <h1 className='intro-slides-text'>Download my resume.</h1>
               <IonButton
-                href='`${process.env.PUBLIC_URL}/assets/shady_alzayat.pdf`'
+                href='/assets/shady_alzayat.pdf'
                 download='shady_alzayat.pdf'
                 fill='outline'
                 color='dark'
